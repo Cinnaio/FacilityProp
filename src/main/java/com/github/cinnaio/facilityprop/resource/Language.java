efd7bfa15d415be927fcdbf74320d95bd2233f1f
curii <ersynn@outlook.com>
2023-09-06 20:46:21 +0800

    add: 修改语言文件

--- src/main/java/com/github/cinnaio/facilityprop/resource/Language.java
+++ src/main/java/com/github/cinnaio/facilityprop/resource/Language.java
@@ -9,4 +9,6 @@ public class Language {
     private static YamlConfiguration file = YamlConfiguration.loadConfiguration(new File(FacilityProp.instance.getDataFolder().getPath() + File.separator + "zh_cn.yml"));
 
     public static String successful_teapan = file.getString("successful_teapan");
+    public static String error_weather = file.getString("error_weather");
+    public static String crafting_teapan = file.getString("crafting_teapan");
 }
