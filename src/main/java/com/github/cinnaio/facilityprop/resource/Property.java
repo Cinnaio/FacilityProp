3de3f489d2d20814f0b112c6af256249f69a4967
curii <ersynn@outlook.com>
2023-09-06 20:49:53 +0800

    add: 增加了茶盘的天气条件及等待时间

--- src/main/java/com/github/cinnaio/facilityprop/resource/Property.java
+++ src/main/java/com/github/cinnaio/facilityprop/resource/Property.java
@@ -1,8 +1,7 @@
 package com.github.cinnaio.facilityprop.resource;
 
 import com.github.cinnaio.facilityprop.FacilityProp;
-import com.github.cinnaio.facilityprop.utils.HexCodeUtils;
-import com.github.cinnaio.facilityprop.utils.MessageUtils;
+import com.github.cinnaio.facilityprop.utils.*;
 import dev.lone.itemsadder.api.CustomBlock;
 import dev.lone.itemsadder.api.ItemsAdder;
 import org.bukkit.Bukkit;
@@ -10,25 +9,21 @@ import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
 public class Property {
-    public static void PropTeapan(Player p, Location loc) {
-        Bukkit.getScheduler().runTaskLater(FacilityProp.instance, new Runnable() {
-            @Override
-            public void run() {
-                CustomBlock.place("grxi:teapan_wet", loc);
-            }
+    public static void PropTeapan(Player p, Location loc, String n) {
+        Bukkit.getScheduler().runTaskLater(FacilityProp.instance, () -> {
+            CustomBlock.place("grxi:teapan_wet", loc);
         }, 3L);
-        MessageUtils.sendActionBar(p, HexCodeUtils.translateHexCodes("&#", "", "&#FBB6B6" + Language.successful_teapan).replace("&", "§"));
-        Bukkit.getScheduler().runTaskLater(FacilityProp.instance, new Runnable() {
-            @Override
-            public void run() {
-                CustomBlock.place("grxi:teapan", loc);
-            }
-        }, 20L);
-        Bukkit.getScheduler().runTaskLater(FacilityProp.instance, new Runnable() {
-            @Override
-            public void run() {
-                p.getInventory().addItem(ItemsAdder.getCustomItem("grxi:amber"));
-            }
-        }, 23L);
+        MessageUtils.sendActionBar(p, HexCodeUtils.translateHexCodes("&#", "", "&#FBB6B6" + Language.crafting_teapan).replace("&", "§"));
+        Bukkit.getScheduler().runTaskLater(FacilityProp.instance, () -> {
+            CustomBlock.place("grxi:teapan", loc);
+        }, Resource.getTime(n));
+
+        Bukkit.getScheduler().runTaskLater(FacilityProp.instance, () -> {
+            p.getInventory().addItem(ItemsAdder.getCustomItem("grxi:amber"));
+        }, Resource.getTime(n) + 3L);
+
+        Bukkit.getScheduler().runTaskLater(FacilityProp.instance, () -> {
+            MessageUtils.sendActionBar(p, HexCodeUtils.translateHexCodes("&#", "", "&#FBB6B6" + Language.successful_teapan).replace("&", "§"));
+        }, Resource.getTime(n));
     }
 }
