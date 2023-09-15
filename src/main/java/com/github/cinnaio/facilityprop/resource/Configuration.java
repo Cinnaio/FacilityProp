21473b15a789e48ecb99e5f5c10f0f33b13ea89b
curii <ersynn@outlook.com>
2023-09-15 11:59:47 +0800

    optimize: 调整语言文件及部分配置

---
+++ src/main/java/com/github/cinnaio/facilityprop/resource/Configuration.java
@@ -0,0 +1,18 @@
+package com.github.cinnaio.facilityprop.resource;
+
+public class Configuration {
+    public static String facility = "facility.";
+
+    public static String main_hexcode_actionbar = "main.hexcode_actionbar";
+    public static String main_hexcode_bossbar = "main.hexcode_bossbar";
+    public static String main_hexcode_normal = "main.hexcode_normal";
+
+    public static String namespace = ".namespace";
+    public static String acquire_items = ".acquire_items";
+    public static String custom_model_data = ".custom_model_data";
+    public static String amount = ".amount";
+    public static String conditions = ".conditions";
+
+    public static String weather = "weather";
+    public static String permissions = "permissions";
+}
--- src/main/java/com/github/cinnaio/facilityprop/resource/Language.java
+++ src/main/java/com/github/cinnaio/facilityprop/resource/Language.java
@@ -9,8 +9,11 @@ public class Language {
     private static YamlConfiguration file = YamlConfiguration.loadConfiguration(new File(FacilityProp.instance.getDataFolder().getPath() + File.separator + "zh_cn.yml"));
 
     public static String successful_teapan = file.getString("successful_teapan");
+
     public static String error_weather = file.getString("error_weather");
+    public static String error_amount = file.getString("error_amount");
+    public static String error_permission = file.getString("error_permission");
+
     public static String crafting_teapan = file.getString("crafting_teapan");
     public static String crafting_wait_time = file.getString("crafting_wait_time");
-    public static String error_amount = file.getString("error_amount");
 }
