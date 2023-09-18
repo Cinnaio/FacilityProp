2497a53ea1135161fa47e84f9b96c5496fd78109
curii <ersynn@outlook.com>
2023-09-18 17:47:40 +0800

    add: 添加了新的配置节点

--- src/main/java/com/github/cinnaio/facilityprop/resource/Configuration.java
+++ src/main/java/com/github/cinnaio/facilityprop/resource/Configuration.java
@@ -2,16 +2,24 @@ package com.github.cinnaio.facilityprop.resource;
 
 public class Configuration {
     public static String facility = "facility.";
+    public static String re_facility = "facility";
 
     public static String main_hexcode_actionbar = "main.hexcode_actionbar";
     public static String main_hexcode_bossbar = "main.hexcode_bossbar";
     public static String main_hexcode_normal = "main.hexcode_normal";
 
     public static String namespace = ".namespace";
+    public static String target_namespace = ".target_namespace";
     public static String acquire_items = ".acquire_items";
     public static String custom_model_data = ".custom_model_data";
     public static String amount = ".amount";
+    public static String waitting = ".waitting";
     public static String conditions = ".conditions";
+    public static String result_items = ".result_items";
+    public static String result_material = ".material";
+    public static String result_namespace = ".namespace";
+    public static String result_custom = ".custom";
+    public static String result_amount = ".amount";
 
     public static String weather = "weather";
     public static String permissions = "permissions";
--- src/main/java/com/github/cinnaio/facilityprop/resource/Language.java
+++ src/main/java/com/github/cinnaio/facilityprop/resource/Language.java
@@ -1,19 +1,14 @@
 package com.github.cinnaio.facilityprop.resource;
 
-import com.github.cinnaio.facilityprop.FacilityProp;
-import org.bukkit.configuration.file.YamlConfiguration;
-
-import java.io.File;
+import static com.github.cinnaio.facilityprop.ConfigHandler.langYaml;
 
 public class Language {
-    private static YamlConfiguration file = YamlConfiguration.loadConfiguration(new File(FacilityProp.instance.getDataFolder().getPath() + File.separator + "zh_cn.yml"));
+    public static String successful_teapan = langYaml.getString("successful_teapan");
 
-    public static String successful_teapan = file.getString("successful_teapan");
+    public static String error_weather = langYaml.getString("error_weather");
+    public static String error_amount = langYaml.getString("error_amount");
+    public static String error_permission = langYaml.getString("error_permission");
 
-    public static String error_weather = file.getString("error_weather");
-    public static String error_amount = file.getString("error_amount");
-    public static String error_permission = file.getString("error_permission");
-
-    public static String crafting_teapan = file.getString("crafting_teapan");
-    public static String crafting_wait_time = file.getString("crafting_wait_time");
+    public static String crafting_teapan = langYaml.getString("crafting_teapan");
+    public static String crafting_wait_time = langYaml.getString("crafting_wait_time");
 }
