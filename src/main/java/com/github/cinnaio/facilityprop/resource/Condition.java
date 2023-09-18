c592f0914fb9be96fc14733a89b598ca92cb4efe
curii <ersynn@outlook.com>
2023-09-18 17:45:43 +0800

    add: 添加了支持自定义结果物品的方法

--- src/main/java/com/github/cinnaio/facilityprop/resource/Condition.java
+++ src/main/java/com/github/cinnaio/facilityprop/resource/Condition.java
@@ -2,7 +2,9 @@ package com.github.cinnaio.facilityprop.resource;
 
 import com.github.cinnaio.facilityprop.utils.HexCodeUtils;
 import com.github.cinnaio.facilityprop.utils.MessageUtils;
+import dev.lone.itemsadder.api.CustomStack;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerInteractEvent;
@@ -62,7 +64,7 @@ public class Condition {
                 if (item instanceof String && flag) {
                     if (item.equals(sun) || item.equals(rain) || item.equals(snow) || item.equals(storm)) {
                         if (item.equals(getWorldWeather(e.getPlayer()))) {
-                            if (items.size() > 1) {
+                            if (items.size() == 2) {
                                 List<Object> tmp = items.stream().filter(s -> !(s instanceof Integer)).collect(Collectors.toList());
                                 List<Object> temp = tmp.stream().skip(1).collect(Collectors.toList());
 
@@ -113,13 +115,60 @@ public class Condition {
         if (holdAmount >= tmpAmount) {
             if (holdAmount.equals(tmpAmount)) {
                 e.getItem().setAmount(0);
-                Property.PropTeapan(p, loc, n);
+                Property.PropTeapan(p, loc, n, e);
             } else {
                 e.getItem().setAmount(e.getItem().getAmount() - tmpAmount);
-                Property.PropTeapan(p, loc, n);
+                Property.PropTeapan(p, loc, n, e);
             }
         } else {
             MessageUtils.sendActionBar(p, HexCodeUtils.translateHexCodes(Resource.getHexCode(HexCodeUtils.HexCode.ACTIONBAR) + Language.error_amount));
+        }
+    }
+
+    public static void sendResultItems(String n, PlayerInteractEvent e) {
+        Boolean flag = false;
+        Boolean flag1 = false;
+
+        for (List<Object> items : Resource.getAcquireItems(n)) {
+            for (Object item : items) {
+                if (item instanceof Integer) {
+                    if (item.equals(e.getItem().getItemMeta().getCustomModelData())) {
+                        flag = true;
+                        flag1 = true;
+                    }
+                }
+            }
+
+            if (flag) {
+                if (items.size() > 1) {
+                    if (flag1) {
+                        flag1 = !flag1;
+                        continue;
+                    }
+                    List<Object> tmp = items.stream().collect(Collectors.toList());
+
+                    for (Object object : tmp) {
+                        List<Object> temp = (List<Object>) object;
+                        if ((Boolean) temp.get(0)) {
+                            String naspaceData = (String) temp.get(1);
+                            Integer amountData = (Integer) temp.get(2);
+
+                            ItemStack itemStack = CustomStack.getInstance(naspaceData).getItemStack();
+                            itemStack.setAmount(amountData);
+
+                            e.getPlayer().getInventory().addItem(itemStack);
+                        } else {
+                            String materialData = ((String) temp.get(1)).toUpperCase();
+                            Integer amountData = (Integer) temp.get(2);
+
+                            ItemStack itemStack = new ItemStack(Material.getMaterial(materialData), amountData);
+
+                            e.getPlayer().getInventory().addItem(itemStack);
+                        }
+                    }
+                    flag = !flag;
+                }
+            }
         }
     }
 }
