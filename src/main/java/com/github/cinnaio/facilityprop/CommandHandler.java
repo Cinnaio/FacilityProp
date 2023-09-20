42288794747842ffedd61b3d8640a1fdc43b6f55
curii <ersynn@outlook.com>
2023-09-20 11:02:14 +0800

    optimize: 优化了重载命令

--- src/main/java/com/github/cinnaio/facilityprop/CommandHandler.java
+++ src/main/java/com/github/cinnaio/facilityprop/CommandHandler.java
@@ -1,5 +1,6 @@
 package com.github.cinnaio.facilityprop;
 
+import com.github.cinnaio.facilityprop.resource.Language;
 import com.github.cinnaio.facilityprop.utils.HexCodeUtils;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -8,12 +9,18 @@ import org.bukkit.command.CommandSender;
 public class CommandHandler implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        if (args[0].equals("reload")) {
-            ConfigHandler.relaodConfig();
-            sender.sendMessage(HexCodeUtils.translateHexCodes("&8[ &#d9fbb4群隙 &8] &#FBEDF6配置文件已经重载！"));
-            return true;
+        if (args.length == 0) {
+            return false;
         }
 
+        if (sender.hasPermission("facilityprop.admin")) {
+            if (args[0].equals("reload")) {
+                ConfigHandler.relaodConfig();
+                sender.sendMessage(HexCodeUtils.translateHexCodes("&8[ &#d9fbb4群隙 &8] &#FBEDF6配置文件已经重载！"));
+                return true;
+            }
+        }
+        sender.sendMessage(HexCodeUtils.translateHexCodes("&8[ &#d9fbb4群隙 &8] &#FBEDF6所需权限不足！"));
         return false;
     }
 }
